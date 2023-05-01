f6f45a85b9910c735f66d1d0ba115b8c94aeda0b
AliveTube <92478946+AliveTube@users.noreply.github.com>
2023-05-01 18:53:06 +0300

    Update mergeLinkedList.cpp

--- mergeLinkedList.cpp
+++ mergeLinkedList.cpp
@@ -2,7 +2,7 @@
 
 void mergeLinkedList() {}
 singleLinkedList<int> sortedList;
-template <typename T ,typename... heads>
+template <typename T,typename... heads>
 void mergeLinkedList(T head , heads... Heads){
     if(sortedList.isEmpty()){
         while(head != NULL){
@@ -14,14 +14,34 @@ void mergeLinkedList(T head , heads... Heads){
         while(head != NULL){
             sllNode<int>* tempNode = sortedList.returnHead();
             int index = 0;
-            while(head->key >= tempNode->key && tempNode != NULL){
+            while(tempNode != NULL && head->key >= tempNode->key){
                 tempNode=tempNode->Next;
                 index++;
-                if(tempNode==NULL)break;
             }
             sortedList.insertAt(head->key,index);
             head=head->Next;
         }
     }
     mergeLinkedList(Heads...);
+}
+
+int main(){
+    singleLinkedList<int> list , list2 , list3;
+    list.insertAtTail(1);
+    list.insertAtTail(2);
+    list.insertAtTail(3);
+    list.insertAtTail(4);
+
+    list2.insertAtTail(44);
+    list2.insertAtTail(55);
+    list2.insertAtTail(125);
+    list2.insertAtTail(300);
+
+    list3.insertAtTail(3);
+    list2.insertAtTail(34);
+    list2.insertAtTail(37);
+    list2.insertAtTail(59);
+
+    mergeLinkedList(list.returnHead(),list2.returnHead(),list3.returnHead());
+    sortedList.print();
 }
